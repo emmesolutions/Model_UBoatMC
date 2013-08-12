dad141918eeaa0f4e0051a1ec9e4cfa940233daf
Martinelli Michele <michele@webemme.net>
2013-08-12 13:03:26 +0200

    Update Cmd_Rudders.ino
    Update...

--- WM_InoSketch/UBoatMC/Cmd_Rudders.ino
+++ WM_InoSketch/UBoatMC/Cmd_Rudders.ino
@@ -134,21 +134,21 @@ void Cmd_Rudders (){
 
   // 01 Fixed/Straight Direction/Immersion Rudders
   // Parameter:
-// 01 Rudder Direction Port Max Angle
-// 02 Rudder Direction Port Calculation Angle
-// 03 Rudder Direction Port Calculation Angle
-// 04 Rudder Direction Port Min Angle
-// 05 Rudder Immersion Dive 1/3 Angle
-// 06 Rudder Immersion Dive 2/3 Angle
-// 07 Rudder Immersion Dive 3/3 Angle
-// 08 Rudders Reset Default Value
-// 09 Rudder Direction Starboard Max Angle
-// 10 Rudder Direction Starboard Calculation Angle
-// 11 Rudder Direction Starboard Calculation Angle
-// 12 Rudder Direction Starboard Min Angle
-// 13 Rudder Immersion Emersion 1/3 Angle
-// 14 Rudder Immersion Emersion 2/3 Angle
-// 15 Rudder Immersion Emersion 3/3 Angle
+  // 01 Rudder Direction Port Max Angle
+  // 02 Rudder Direction Port Calculation Angle
+  // 03 Rudder Direction Port Calculation Angle
+  // 04 Rudder Direction Port Min Angle
+  // 05 Rudder Immersion Dive 1/3 Angle
+  // 06 Rudder Immersion Dive 2/3 Angle
+  // 07 Rudder Immersion Dive 3/3 Angle
+  // 08 Rudders Reset Default Value
+  // 09 Rudder Direction Starboard Max Angle
+  // 10 Rudder Direction Starboard Calculation Angle
+  // 11 Rudder Direction Starboard Calculation Angle
+  // 12 Rudder Direction Starboard Min Angle
+  // 13 Rudder Immersion Emersion 1/3 Angle
+  // 14 Rudder Immersion Emersion 2/3 Angle
+  // 15 Rudder Immersion Emersion 3/3 Angle
   if (OpCmd_Rd1 [1]){		// Execution OpCmd
     Ang_Clc = Fnc_Rudders_AngClc (1);
     RuddrD_Pos = Rudder_Rst + Ang_Clc;
@@ -310,7 +310,8 @@ static int Fnc_Rudders_AngClc (int Val) {
 
   int Clc = 0;
 
-  if (PrCmd_Rd0 [Val] == 8) { // PrCmd 8 - Default
+  if (PrCmd_Rd0 [Val] == 8) { 
+  // PrCmd 8 - Default
     switch (Val) {
     case 1:    // Fixed/Straight Direction
       Clc = 0;
@@ -336,6 +337,7 @@ static int Fnc_Rudders_AngClc (int Val) {
     }
   }
   else {
+  // PrCmd 1/7 - 9/15
     if (PrCmd_Rd0 [Val] < 8) { // PrCmd < 8 [OPCmd01 Port]
       if (PrCmd_Rd0 [Val] == 7) Clc = Ang_Min;
       if (PrCmd_Rd0 [Val] == 6) Clc = (int((Ang_Max-Ang_Min)/6)*1)+Ang_Min;
