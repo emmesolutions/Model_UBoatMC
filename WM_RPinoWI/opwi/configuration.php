92d75e4386bea8fdf4ce18e56b0adc75889cde4a
emmesolutions <emmesolutions@gmail.com>
2015-04-09 19:35:23 +0200

    Update Rudders Parameters
    Signed-off-by:  <emmesolutions@gmail.com>

--- WM_RPinoWI/opwi/configuration.php
+++ WM_RPinoWI/opwi/configuration.php
@@ -32,8 +32,8 @@ define("INO_OPCMD_A010", "R");
 // 06 Reversing Port Direction	
 define("INO_OPCMD_A020", "6");
 	define("INO_OPPRM_A021", "1");
-	define("INO_OPPRM_A022", "8");
-	define("INO_OPPRM_A023", "5");
+	define("INO_OPPRM_A022", "4");
+	define("INO_OPPRM_A023", "7");
 // NONE		
 define("INO_OPCMD_A060", "X");
 	define("INO_OPPRM_A061", "X");
@@ -58,8 +58,8 @@ define("INO_OPCMD_B010", "S");
 // 04 90° Turn Port Direction	
 define("INO_OPCMD_B020", "4");
 	define("INO_OPPRM_B021", "1");
-	define("INO_OPPRM_B022", "8");
-	define("INO_OPPRM_B023", "5");
+	define("INO_OPPRM_B022", "4");
+	define("INO_OPPRM_B023", "7");
 // NONE	
 define("INO_OPCMD_B060", "X");
 	define("INO_OPPRM_B061", "X");
@@ -84,8 +84,8 @@ define("INO_OPCMD_C010", "T");
 // 02 Quadrant Turn Port Direction	
 define("INO_OPCMD_C020", "2");
 	define("INO_OPPRM_C021", "1");
-	define("INO_OPPRM_C022", "8");
-	define("INO_OPPRM_C023", "5");
+	define("INO_OPPRM_C022", "4");
+	define("INO_OPPRM_C023", "7");
 // Engine Astern	
 define("INO_OPCMD_C030", "8");
 	define("INO_OPPRM_C031", "1");
@@ -119,13 +119,13 @@ define("INO_OPCMD_C080", "X");
 // Column DE
 // 01 Fixed/Reset Rudders Direction
 define("INO_OPCMD_DE20", "1");
-	define("INO_OPPRM_DE21", "2");
+	define("INO_OPPRM_DE21", "1");
 	define("INO_OPPRM_DE22", "4");
-	define("INO_OPPRM_DE23", "6");
+	define("INO_OPPRM_DE23", "7");
 	define("INO_OPPRM_DE24", "8");	
-	define("INO_OPPRM_DE25", "A");
+	define("INO_OPPRM_DE25", "9");
 	define("INO_OPPRM_DE26", "C");
-	define("INO_OPPRM_DE27", "E");
+	define("INO_OPPRM_DE27", "F");
 // Engine Stop
 define("INO_OPCMD_DE30", "0");
 	define("INO_OPPRM_DE31", "1");
@@ -159,8 +159,8 @@ define("INO_OPCMD_F010", "N");
 // 03 Quadrant Turn Starboard Direction
 define("INO_OPCMD_F020", "3");
 	define("INO_OPPRM_F021", "1");
-	define("INO_OPPRM_F022", "8");
-	define("INO_OPPRM_F023", "5");
+	define("INO_OPPRM_F022", "4");
+	define("INO_OPPRM_F023", "7");
 // Engine Ahead	
 define("INO_OPCMD_F030", "9");
 	define("INO_OPPRM_F031", "1");
@@ -200,8 +200,8 @@ define("INO_OPCMD_G010", "O");
 // 05 90° Turn Starboard Direction
 define("INO_OPCMD_G020", "5");
 	define("INO_OPPRM_G021", "1");
-	define("INO_OPPRM_G022", "8");
-	define("INO_OPPRM_G023", "5");
+	define("INO_OPPRM_G022", "4");
+	define("INO_OPPRM_G023", "7");
 // NONE		
 define("INO_OPCMD_G060", "X");
 	define("INO_OPPRM_G061", "X");
@@ -226,8 +226,8 @@ define("INO_OPCMD_H010", "P");
 // 07 Reversing Starboard Direction	
 define("INO_OPCMD_H020", "7");
 	define("INO_OPPRM_H021", "1");
-	define("INO_OPPRM_H022", "8");
-	define("INO_OPPRM_H023", "5");
+	define("INO_OPPRM_H022", "4");
+	define("INO_OPPRM_H023", "7");
 // NONE		
 define("INO_OPCMD_H060", "X");
 	define("INO_OPPRM_H061", "X");
